d2d78a4e89c2f72a804cdee523f5be85722cb8c4
Foglio <foglio1024@gmail.com>
2026-04-18 16:06:42 +0200

    write log file if app fails to start

--- PieLauncher.Avalonia/Program.cs
+++ PieLauncher.Avalonia/Program.cs
@@ -1,14 +1,24 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 
 namespace PieLauncher.Avalonia;
 
 internal static class Program
 {
     [STAThread]
-    public static void Main(string[] args) 
-        => BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        try
+        {
+            BuildAvaloniaApp()
+                    .StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            File.WriteAllText("error.log", ex.ToString());
+        }
+    }
 
     private static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
